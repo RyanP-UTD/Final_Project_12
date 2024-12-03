431f39af3a942c7206ce6bd18e08550d56d9fb59
Ryan Preetham <Ryan.Preetham@UTDallas.edu>
2024-12-03 17:31:24 -0600

    Added the bug image on top of the red box for a reference

--- src/take_1.py
+++ src/take_1.py
@@ -1,4 +1,5 @@
 import pygame
+import os
 
 def main():
     pygame.init()
@@ -17,6 +18,18 @@ def main():
     border_thickness = 4
     border_rectangle = pygame.Rect(50, 70, 800, 600) # should posit 40 by 70px and the rect by 800 by 600px
 
+    # red box
+    red_box_color = 'Red'
+    red_box_size = (50, 60)
+    red_box_x = (900 - red_box_size[0]) // 2
+    red_box_y = (700 - red_box_size[0]) // 2
+    red_box = pygame.Rect(red_box_x, red_box_y, * red_box_size)
+
+    # Pull Image
+    image_path = os.path.join('..', 'docs', 'Bug.png')
+    image = pygame.image.load(image_path) #Should load an image
+    image = pygame.transform.scale(image, red_box_size)
+
     # Main loop
     running = True
     while running:
@@ -29,6 +42,10 @@ def main():
         pygame.draw.rect(screen, border_color, border_rectangle, border_thickness)
 
         screen.blit(title_text, title_rectangle)
+
+        pygame.draw.rect(screen, red_box_color, red_box)
+
+        screen.blit(image, red_box.topleft)
 
         pygame.display.flip()
 
