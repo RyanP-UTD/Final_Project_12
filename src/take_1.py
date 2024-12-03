417537ec727e0a372afb46697fb32326a4da3780
Ryan Preetham <Ryan.Preetham@UTDallas.edu>
2024-12-02 21:25:46 -0600

    Added the title, Encountered some issues but fixed

--- src/take_1.py
+++ src/take_1.py
@@ -6,8 +6,14 @@ def main():
     screen = pygame.display.set_mode((900, 700))
     pygame.display.set_caption("Bug Swatter")
 
+    # Title
+    pygame.font.init()
+    font = pygame.font.Font(None, 48)
+    title_text = font.render("Bug Swatter", True, ('White'))
+    title_rectangle = title_text.get_rect(center = (450, 40))
+
     # border line
-    border_color = ('White')
+    border_color = 'White'
     border_thickness = 4
     border_rectangle = pygame.Rect(50, 70, 800, 600) # should posit 40 by 70px and the rect by 800 by 600px
 
@@ -19,8 +25,10 @@ def main():
                 running = False
 
         screen.fill('Black')
-        
+
         pygame.draw.rect(screen, border_color, border_rectangle, border_thickness)
+
+        screen.blit(title_text, title_rectangle)
 
         pygame.display.flip()
 
